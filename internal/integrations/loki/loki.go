3fbe8fbd1ff67ff6b74712af10148a749619e5ff
Hammed Adigun <adigunhammed.lekan@gmail.com>
2025-02-09 22:20:11 +0100

    add loki client to push and query logs

--- internal/integrations/loki/loki.go
+++ internal/integrations/loki/loki.go
@@ -1,1 +1,100 @@
 package loki
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"sarabi/internal/integrations"
+	"sarabi/internal/types"
+	"strconv"
+	"time"
+)
+
+type Client interface {
+	Push(ctx context.Context, payloads map[string][]types.Batch) error
+	Ready(ctx context.Context) error
+	Query(ctx context.Context, filter types.Filter) ([]types.LogEntry, error)
+}
+
+type client struct {
+	httpClient integrations.HttpClient
+}
+
+const lokiUrl = "http://localhost:3100/loki/api/v1/"
+
+func NewClient() Client {
+	return &client{
+		httpClient: integrations.NewHttpClient(lokiUrl),
+	}
+}
+
+func (c client) Push(ctx context.Context, payloads map[string][]types.Batch) error {
+	for _, batches := range payloads {
+		streams := make([]Stream, 0)
+		for _, next := range batches {
+			s := Stream{
+				Stream: map[string]string{
+					"environment": next.Deployment.Environment,
+					"app":         next.Deployment.ApplicationID.String(),
+				},
+				Values: [][2]string{
+					{
+						next.Log.Ts, next.Log.Line(),
+					},
+				},
+			}
+			streams = append(streams, s)
+		}
+
+		payload := Payload{Streams: streams}
+		err := c.httpClient.Do(ctx, "POST", "push", payload, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c client) Ready(ctx context.Context) error {
+	return c.httpClient.Do(ctx, "GET", "http://localhost:3100/ready", nil, nil)
+}
+
+func (c client) Query(ctx context.Context, f types.Filter) ([]types.LogEntry, error) {
+	u := url.Values{}
+	u.Add("limit", "1000")
+	u.Add("direction", "forward")
+	if f.Since != "" {
+		u.Add("since", f.Since)
+	}
+
+	if !f.Start.IsZero() {
+		u.Add("start", strconv.FormatInt(f.Start.UnixNano(), 10))
+
+		if f.End.IsZero() {
+			u.Add("end", strconv.FormatInt(time.Now().UnixNano(), 10))
+		} else {
+			u.Add("end", strconv.FormatInt(f.End.UnixNano(), 10))
+		}
+	}
+
+	query := fmt.Sprintf(`{app="%s", environment="%s"}`, f.ApplicationID, f.Environment)
+	u.Add("query", query)
+
+	queryUrl := "query_range?" + u.Encode()
+	result := &QueryResult{}
+	err := c.httpClient.Do(ctx, "GET", queryUrl, nil, result)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := make([]types.LogEntry, 0)
+	for _, next := range result.Data.Result {
+		for _, value := range next.Values {
+			resp = append(resp, types.LogEntry{
+				Log: value[1],
+				Ts:  value[0],
+			})
+		}
+	}
+	return resp, nil
+}
--- internal/integrations/loki/types.go
+++ internal/integrations/loki/types.go
@@ -1,1 +1,133 @@
 package loki
+
+import "gopkg.in/yaml.v3"
+
+type Config struct {
+	AuthEnabled   bool          `yaml:"auth_enabled"`
+	Server        ServerConfig  `yaml:"server"`
+	Common        CommonConfig  `yaml:"common"`
+	SchemaConfig  SchemaConfig  `yaml:"schema_config"`
+	StorageConfig StorageConfig `yaml:"storage_config"`
+}
+
+type ServerConfig struct {
+	HTTPListenPort int `yaml:"http_listen_port"`
+}
+
+type CommonConfig struct {
+	Ring              RingConfig `yaml:"ring"`
+	ReplicationFactor int        `yaml:"replication_factor"`
+	PathPrefix        string     `yaml:"path_prefix"`
+}
+
+type RingConfig struct {
+	InstanceAddr string        `yaml:"instance_addr"`
+	KVStore      KVStoreConfig `yaml:"kvstore"`
+}
+
+type KVStoreConfig struct {
+	Store string `yaml:"store"`
+}
+
+type SchemaConfig struct {
+	Configs []SchemaConfigItem `yaml:"configs"`
+}
+
+type SchemaConfigItem struct {
+	From        string      `yaml:"from"`
+	Store       string      `yaml:"store"`
+	ObjectStore string      `yaml:"object_store"`
+	Schema      string      `yaml:"schema"`
+	Index       IndexConfig `yaml:"index"`
+}
+
+type IndexConfig struct {
+	Prefix string `yaml:"prefix"`
+	Period string `yaml:"period"`
+}
+
+type StorageConfig struct {
+	TSDBShipper TSDBShipperConfig `yaml:"tsdb_shipper"`
+	AWS         *AWSConfig        `yaml:"aws,omitempty"`
+	FileSystem  *FileSystemConfig `yaml:"filesystem,omitempty"`
+}
+
+type TSDBShipperConfig struct {
+	ActiveIndexDirectory string `yaml:"active_index_directory"`
+	CacheLocation        string `yaml:"cache_location"`
+}
+
+type AWSConfig struct {
+	S3               string `yaml:"s3"`
+	S3ForcePathStyle bool   `yaml:"s3forcepathstyle"`
+}
+
+type FileSystemConfig struct {
+	Directory string `json:"directory"`
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		AuthEnabled: false,
+		Server: ServerConfig{
+			HTTPListenPort: 3100,
+		},
+		Common: CommonConfig{
+			Ring: RingConfig{
+				InstanceAddr: "127.0.0.1",
+				KVStore: KVStoreConfig{
+					Store: "inmemory",
+				},
+			},
+			ReplicationFactor: 1,
+			PathPrefix:        "/var/loki",
+		},
+		SchemaConfig: SchemaConfig{
+			Configs: []SchemaConfigItem{
+				{
+					From:        "2020-05-15",
+					Store:       "tsdb",
+					ObjectStore: "s3",
+					Schema:      "v13",
+					Index: IndexConfig{
+						Prefix: "index_",
+						Period: "24h",
+					},
+				},
+			},
+		},
+		StorageConfig: StorageConfig{
+			TSDBShipper: TSDBShipperConfig{
+				ActiveIndexDirectory: "/var/loki/index",
+				CacheLocation:        "/var/loki/cache",
+			},
+			AWS: &AWSConfig{
+				S3ForcePathStyle: true,
+			},
+		},
+	}
+}
+
+func DefaultConfigYaml() ([]byte, error) {
+	return yaml.Marshal(DefaultConfig())
+}
+
+type Payload struct {
+	Streams []Stream
+}
+
+type Stream struct {
+	Stream map[string]string `json:"stream"`
+	Values [][2]string       `json:"values"`
+}
+
+type QueryResult struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Stream map[string]string `json:"stream"`
+			Values [][]string        `json:"values"`
+		} `json:"result"`
+	} `json:"data"`
+}
